41b0f5e5eac4bcb8342f0f1e7411f3b03eebedb5
Tohir Bahromov <tohirbaxromov13@gmail.com>
2023-07-06 16:43:58 +0500

    updated styles

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -128,7 +128,7 @@ export default function Chat() {
   return (
     <>
       <div className="flex h-screen relative">
-        <div className={"contacts bg-white w-1/3 flex flex-col " + (showMenu ? "left-0" : "")}>
+        <div className={"contacts bg-white w-1/3 flex flex-col " + (showMenu ? "left-[0%]" : "")}>
           <div className="logo">
             <Logo />
           </div>
@@ -184,7 +184,7 @@ export default function Chat() {
             </button>
           </div>
         </div>
-        <div className="mobile flex flex-col">
+        <div className="mobile flex flex-col w-full">
           <div className="mobile-navbar items-center justify-between p-4 bg-white text-blue-500 hidden">
             <div className="text-blue-500 font-bold flex items-center gap-2 p-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
@@ -199,7 +199,7 @@ export default function Chat() {
               </svg>
             </div>
           </div>
-          <div className="chat-screen bg-blue-50 w-2/3 p-2 flex flex-col flex-grow">
+          <div className="chat-screen bg-blue-50 p-2 flex flex-col flex-grow">
             <div className="flex-grow">
               {!selectedUserId &&
               (
