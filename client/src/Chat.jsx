778a76b3df25b3d82185fc81b31cf03006c0530c
Tohir Bahromov <tohirbaxromov13@gmail.com>
2023-07-06 16:49:31 +0500

    updated styles

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -128,7 +128,7 @@ export default function Chat() {
   return (
     <>
       <div className="flex h-screen relative">
-        <div className={"contacts bg-white w-1/3 flex flex-col " + (showMenu ? "left-[0%]" : "")}>
+        <div className={"contacts bg-white w-1/3 flex flex-col " + (showMenu ? "left-[0%]" : "left-[-100%]")}>
           <div className="logo">
             <Logo />
           </div>
@@ -171,7 +171,7 @@ export default function Chat() {
               ))}
             </div>
           </div>
-          <div className="text-center p-2 flex items-center justify-center">
+          <div className="text-center p-3 flex items-center justify-center">
             <span className="mr-2 text-gray-600 flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                 <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
